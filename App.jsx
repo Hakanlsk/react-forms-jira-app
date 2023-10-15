63439ec6bccfada945982efc1419a788eee12a22
Hakan Işık <123507532+Hakanlsk@users.noreply.github.com>
2023-10-15 22:29:12 +0300

    Add files via upload

--- App.jsx
+++ App.jsx
@@ -1,30 +1,53 @@
 import './App.css'
 import TaskCreate from './components/TaskCreate'
 import TaskList from './components/TaskList'
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
+import axios from 'axios';
+
 
 function App() {
   const [tasks, setTasks] = useState([]);
   //TaskCreate.jsx' te onCreate propsuna yolladğımız title ve taskDesc
-  const createTask = (title, taskDesc) =>{
-    const createTasks = [
-      ...tasks,{                                  //spread sayesinde var olan arr kopyalanır yeni task eklenip yeni arr olur
-        id:Math.round(Math.random() * 999999),
-        title,                                    //title : title, key ile value değeri aynıysa bu şekilde yazılabilir
-        taskDesc                                   
-      }
-    ]
-    setTasks(createTasks);
+  const createTask = async (title, taskDesc) =>{
+
+  //axios POST isteği
+  const response = await axios.post('http://localhost:3000/tasks', {
+    title,  //title:title
+    taskDesc
+  });
+
+  const createdTasks = [
+      ...tasks,                                   //spread sayesinde var olan arr kopyalanır yeni task eklenip yeni arr olur
+      response.data                               //apiden gelen response ile task oluşturduk              
+    ];
+    setTasks(createdTasks);
+  };
+
+  //GET isteğimiz için fonksiyon
+  const fetchTasks = async () => {
+    const response = await axios.get('http://localhost:3000/tasks')
+    debugger
+    setTasks(response.data)
   }
 
-  const deleteTaskById = (id) => {
+  //USEFFECT ile GET isteği
+  useEffect(()=>{
+    fetchTasks();
+  }, []);
+
+  const deleteTaskById = async (id) => {
+    await axios.delete(`http://localhost:3000/tasks/${id}`);
     const afterDeletingTasks = tasks.filter((task)=>{   //id'si sil butonuna basılan task ile aynı olmayanlar 
       return task.id !== id;
     })
     setTasks(afterDeletingTasks);
   }
 
-  const editTaskById = (id, updatedTitle, updatedTaskDesc) => {
+  const editTaskById = async (id, updatedTitle, updatedTaskDesc) => {
+    await axios.put(`http://localhost:3000/tasks/${id}`,{
+      title:updatedTitle,
+      taskDesc:updatedTaskDesc
+    })
     //id ile, güncelenen taskı tespit edilip yeni parametrelerle içeriği güncelleyeceğiz yeni arr oluşacak
     const updatedTasks = tasks.map((task) => {
       if(task.id === id){
